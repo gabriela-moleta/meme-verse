f9dbe94afb192843e0d2d7ad95efc4543a06020a
Gabriela Moleta <gabriela.moleta@aluno.senai.br>
2025-04-03 14:21:26 -0300

    feat: create footer

--- meme-verse/src/app/page.jsx
+++ meme-verse/src/app/page.jsx
@@ -1,12 +1,14 @@
 import Header from './components/header';
-
+import Footer from './components/footer';
+import HeroSection from './components/heroSection';
 
 
 export default function Home() {
   return (
     <div>
       <Header />
-    
+    <Footer />
+    <HeroSection />
     
 
     </div>
